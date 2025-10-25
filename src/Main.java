89d78280f4c344d8fa90c0efedacc3aff8f9ce39
rishalgawade <66425690+rishalgawade@users.noreply.github.com>
2025-10-25 10:51:11 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Starting program...");
-        String text = null;
-        System.out.println("Length of text: " + text.length()); // 💥 NullPointerException
+        String text = "abc";
+        System.out.println("Length of text: " + text); // 💥 NullPointerException
         System.out.println("End of program.");
     }
 }
