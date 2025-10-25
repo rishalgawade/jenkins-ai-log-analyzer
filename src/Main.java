b539a0655f6ba6f6246f2af8ee8a2b2d7e7de30b
rishalgawade <66425690+rishalgawade@users.noreply.github.com>
2025-10-25 10:58:06 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Starting program...");
         String text = "abc";
-        System.out.println("Length of text: " + text); // 💥 NullPointerException
+        System.out.println("Length of text: " + text); 
         System.out.println("End of program.");
     }
 }
 
-//Testing jenkins
+
