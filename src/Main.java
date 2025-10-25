f0525132f8e59dc46d9adf8ccd68695d283b3267
rishalgawade <66425690+rishalgawade@users.noreply.github.com>
2025-10-25 11:37:43 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Starting program...");
         String text = null;
-        System.out.println("Length of text: " + text.lenghtj()); 
+        System.out.println("Length of text: " + text.lenght()); 
         System.out.println("End of program.");
     }
 }
